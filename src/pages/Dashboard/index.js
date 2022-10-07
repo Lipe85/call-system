8d44fcbfba9af5b6f239dc1374c1a585c0bc096b
Lipe <feventurelli@hotmail.com>
2022-10-06 21:33:20 -0300

    Criado o header e o componente title

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,12 +2,16 @@
 import {useContext} from 'react';
 import { AuthContext } from '../../contexts/auth';
 
+import Header from '../../components/Header';
+
 export default function Dashboard(){
 
     const { signOut } = useContext(AuthContext);
 
     return(
         <div>
+            <Header />
+
             <h1>Página Dashboard</h1>
             <button onClick={()=>signOut()}>Fazer logout</button>
         </div>
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,9 @@ import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+
 import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
 
 
 export default function Routes(){
@@ -14,6 +16,7 @@ export default function Routes(){
             <Route exact path="/register" component={SignUp} />
 
             <Route exact path="/dashboard" component={Dashboard} isPrivate />
+            <Route exact path="/profile" component={Profile} isPrivate/>
         </Switch>
     )
 }
